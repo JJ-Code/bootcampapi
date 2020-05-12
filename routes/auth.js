6095a4d6cb638bb15fdcdc455ab795f82ed7c0a7
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-05-11 17:45:46 -0700

    finished auth controller

--- routes/auth.js
+++ routes/auth.js
@@ -2,33 +2,33 @@ const express = require('express');
 const {
   register,
   login,
-  getMe, 
-forgotPassword
+  getMe,
+  logout,
+  forgotPassword,
+  resetPassword,
+  updateDetails,
+  updatePassword
 } = require('../controllers/auth');
+
 //Auth valitated methods with tokens
-const { protect, authorize } = require('../middleware/auth');
+const { protect } = require('../middleware/auth');
 
 
 
 const router = express.Router();
 
-//const { protect } = require('../middleware/auth');
+
 
 router.post('/register', register);
 router.post('/login', login);
-// router.get('/logout', logout);
+router.get('/logout', logout);
 router.get('/me', protect, getMe);
-// router.put('/updatedetails', protect, updateDetails);
-// router.put('/updatepassword', protect, updatePassword);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
-// router.put('/resetpassword/:resettoken', resetPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
 
 
-  // logout,
-  // getMe,
-  // forgotPassword,
-  // resetPassword,
-  // updateDetails,
-  // updatePassword
+
